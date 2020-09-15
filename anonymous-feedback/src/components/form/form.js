6847370f5d4c9d1a97e79042d7d91c439bc8f5bc
aidan-parkinson <aidan.parkinson@gmail.com>
2020-09-15 10:40:01 +0100

    + macaddress

--- anonymous-feedback/src/components/form/form.js
+++ anonymous-feedback/src/components/form/form.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+import './form.css';
 
 import StarRating from '../affective-response/star-rating';
+
+var macaddress = require('macaddress');
 
 class Form extends React.Commponent {
   constructor(props) {
     super(props);
     this.state = {
       rating: null,
-      description: null
+      description: null,
+      macaddress: macaddress.one
     };
   }
 
@@ -27,29 +30,33 @@ class Form extends React.Commponent {
   };
 
   render() {
-    return(
-      <div className="form-input">
-        <textarea
-          name="description"
-          id="description"
-          onChange={this.handleChange}
-          helperText="Provide a description of your experience here..."
-        />
-      </div>
-      <div className="actions">
-        <button type="submit" onClick={this.broadcastFeedback}>
-          Submit feedback
-        </button>
-      </div>
-      <div className="label-unsatisfied">Unsatisfied</div>
-      <div className="label-satisfied">Satisfied</div>
-      <div className="form-input-rating">
-        <StarRating
-          numberOfStars="5"
-          currentRating=null
-          onClick={this.setRating}
-        />
-      </div>
-    )
+  //  return(
+      //<>
+        //<div className="form-input">
+        //   <textarea
+        //     name="description"
+        //     id="description"
+        //     onChange={this.handleChange}
+        //     helperText="Provide a description of your experience here..."
+        //   />
+        // </div>
+        // <div className="actions">
+        //   <button type="submit" onClick={this.broadcastFeedback}>
+        //     Submit feedback
+        //   </button>
+        // </div>
+        // <div className="label-unsatisfied">Unsatisfied</div>
+        // <div className="label-satisfied">Satisfied</div>
+        // <div className="form-input-rating">
+        //   <StarRating
+        //     numberOfStars="5"
+        //     currentRating={null}
+        //     onClick={this.setRating}
+        //   />
+        //</div>
+      //</>
+    // );
   }
 }
+
+export default Form;
