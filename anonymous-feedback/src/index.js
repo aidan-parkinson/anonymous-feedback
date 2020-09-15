458b647754659a88c7290840b6a91c9462118b9b
aidan-parkinson <aidan.parkinson@gmail.com>
2020-09-15 13:25:37 +0100

    + MQTT

--- anonymous-feedback/src/index.js
+++ anonymous-feedback/src/index.js
@@ -5,10 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Connector } from 'mqtt-react';
 
+var options = {
+  port: 8883,
+  host: '35.176.252.212',
+  // key: KEY,
+  cert: '/etc/ca-certificates/learning-iot-ca.crt',
+  rejectUnauthorized: false,
+  // The CA list will be used to determine if server is authorized
+  // ca: TRUSTED_CA_LIST,
+  protocol: 'mqtts'
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Connector mqttProps="ws://test.mosca.io/">
+    <Connector mqttProps={options}>
       <App />
     </Connector>
   </React.StrictMode>,
