e055ec29f87fa9f3d88d6d8ccc8dcb47552adda6
aidan-parkinson <aidan.parkinson@gmail.com>
2020-09-18 22:10:23 +0100

    sorting out form

--- anonymous-feedback/src/components/form/form.js
+++ anonymous-feedback/src/components/form/form.js
@@ -4,6 +4,29 @@ import './form.css';
 import StarRating from '../affective-response/star-rating';
 import mqtt from 'mqtt';
 
+var macaddress = require('macaddress')
+var fs = require('fs')
+
+var thisMacaddress = macaddress
+
+var caFile = fs.readFileSync('/etc/ca-certificates/learning-iot-ca.crt')
+
+var options = {
+    // port: 8883,
+    // host: '35.176.252.212',
+    // key: KEY,
+    ca: caFile,
+    rejectUnauthorized: false,
+    // The CA list will be used to determine if server is authorized
+    // protocol: 'mqtts'
+  }
+
+client  = mqtt.connect("mqtts://35.176.252.212:8883", options);
+console.log("connected flag  " + client.connected);
+
+client.on("connect",function(){
+console.log("connected  "+ client.connected);
+
 class Form extends React.Component {
   constructor(props) {
     super(props);
@@ -12,37 +35,6 @@ class Form extends React.Component {
       description: null,
     };
   }
-
-  componentDidMount(){
-    this.macaddress = require('macaddress').then(
-      this.fs = require('fs')
-    ).then(
-      this.thisMacaddress = this.macaddress
-    ).then(
-      this.caFile = this.fs.readFileSync('/etc/ca-certificates/learning-iot-ca.crt')
-    ).then
-    (
-      this.options = {
-        // port: 8883,
-        // host: '35.176.252.212',
-        // key: KEY,
-        ca: this.caFile,
-        rejectUnauthorized: false,
-        // The CA list will be used to determine if server is authorized
-        // protocol: 'mqtts'
-      }
-    )};
-
-  connect = props => {
-    useMqtt(() => {
-      this.client  = this.mqtt.connect("mqtts://35.176.252.212:8883", this.options);
-      console.log("connected flag  " + this.client.connected);
-
-      this.client.on("connect",function(){
-      console.log("connected  "+ this.client.connected);
-      });
-    });
-  };
 
   handleChange = ev => {
     this.setState({
@@ -55,7 +47,7 @@ class Form extends React.Component {
   };
 
   broadcastFeedback = () => {
-    this.client.publish(`anonymous-feedback/${this.thisMacaddress}/json`, {likert_score: this.state.rating, description: this.state.description});
+    client.publish(`anonymous-feedback/${this.thisMacaddress}/json`, {likert_score: this.state.rating, description: this.state.description});
     this.setState({ rating: null, description: null});
   };
 
