eef6d61ee81166f46cf8f8ba070f1ff5204a6d06
aidan-parkinson <aidan.parkinson@gmail.com>
2020-09-19 14:57:23 +0100

    - routing

--- anonymous-feedback/server/index.js
+++ anonymous-feedback/server/index.js
@@ -11,49 +11,29 @@ var express = require('express');
 const PORT = 3000;
 const path = require('path');
 
-// initialize the application and create the routes
+// initialize the application
 const app = express();
-const router = express.Router();
 
-// root (/) should always serve our server rendered page
-router.use('^/$', function (req, res, next) {
+// ...
 
-    // point to the html file created by CRA's build tool
-    const filePath = path.resolve(__dirname, '..', '..', 'build', 'index.html');
+app.get('/', (req, res) => {
+  const app = ReactDOMServer.renderToString(<App />);
 
-    fs.readFile(filePath, 'utf8', (err, htmlData) => {
-        if (err) {
-            console.error('err', err);
-            return res.status(404).end()
-        }
-
-        // render the app as a string
-        const html = ReactDOMServer.renderToString(<App />);
-
-        // inject the rendered app into our html and send it
-        return res.send(
-            htmlData.replace(
-                '<div id="root"></div>',
-                `<div id="root">${html}</div>`
-            )
-        );
-    });
-});
-
-// other static resources should just be served as they are
-router.use(express.static(
-    path.resolve(__dirname, '..', 'build'),
-    { maxAge: '30d' },
-));
-
-// tell the app to use the above rules
-app.use(router);
-
-// start the app
-app.listen(PORT, (error) => {
-    if (error) {
-        return console.log('something bad happened', error);
+  const indexFile = path.resolve('./build/index.html');
+  fs.readFile(indexFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, better luck next time!');
     }
 
-    console.log("listening on " + PORT + "...");
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+    );
+  });
+});
+
+app.use(express.static('./build'));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
