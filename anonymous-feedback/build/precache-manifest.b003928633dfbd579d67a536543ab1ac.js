6ed45c70ad462a2c5a70b8b28e6fcbd7096f7c74
aidan-parkinson <aidan.parkinson@gmail.com>
2020-09-29 10:39:47 +0100

    certificate check tested

---
+++ anonymous-feedback/build/precache-manifest.b003928633dfbd579d67a536543ab1ac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67769694daaa06897614c996e96eded1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc0457e1c0ac4414511f",
+    "url": "/static/css/3.f452fd33.chunk.css"
+  },
+  {
+    "revision": "881d5a430828cd063ce0",
+    "url": "/static/css/main.2410cedc.chunk.css"
+  },
+  {
+    "revision": "2466ab1e84cad8ea6b0e",
+    "url": "/static/js/2.7cc3eed6.chunk.js"
+  },
+  {
+    "revision": "fba514274c1d2989b99880f25d0a3c7d",
+    "url": "/static/js/2.7cc3eed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0457e1c0ac4414511f",
+    "url": "/static/js/3.d7751214.chunk.js"
+  },
+  {
+    "revision": "21f9d8f455236ea444b9",
+    "url": "/static/js/4.ba410aaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/4.ba410aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "881d5a430828cd063ce0",
+    "url": "/static/js/main.9786b64f.chunk.js"
+  },
+  {
+    "revision": "74f18aa74f1c41ed9118",
+    "url": "/static/js/runtime-main.43f51260.js"
+  }
+]);
--- anonymous-feedback/src/components/form/formWrapper.js
+++ anonymous-feedback/src/components/form/formWrapper.js
@@ -1,24 +1,12 @@
 import * as fs from 'fs';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from "./form";
-
-// const getCert = () => {
-//   try {
-//     var caFile = fs.readFileSync('/etc/ssl/certs/learning-iot-ca.crt');
-//   }
-//   catch(e) {
-//     render() {
-//       return (
-//         <div>test</div>
-//       )}
-//     }
-//   }
 
 const FormWrapper = () => {
   const [caFile, setCaFile] = useState(null);
 
   useEffect (() => {
-    const ca = null;
+    let ca = null;
 
     try {
       ca = fs.readFileSync('/etc/ssl/certs/learning-iot-ca.crt');
@@ -30,17 +18,16 @@ const FormWrapper = () => {
       setCaFile(ca);
     }
   }, [])
-  // const caFile = fs.readFileSync('/etc/ssl/certs/learning-iot-ca.crt');
 
   console.log(caFile);
 
-if (!caFile) {
-  return(<div>loading</div>)
-}
+  if (!caFile) {
+    return(<div>loading</div>)
+  }
 
-return (
-  <Form caFile={caFile}/>
-  )
+  return (
+    <Form caFile={caFile}/>
+    )
 }
 
 export default FormWrapper;
