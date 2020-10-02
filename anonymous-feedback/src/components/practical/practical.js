6334c025db83f5c69fbdc068f027febbafee2376
aidan-parkinson <aidan.parkinson@gmail.com>
2020-10-02 19:21:07 +0100

    practical videos included and lazy loading

--- anonymous-feedback/src/components/practical/practical.js
+++ anonymous-feedback/src/components/practical/practical.js
@@ -4,11 +4,18 @@ import React, { Component } from 'react';
 import Form from '../form/form';
 import Tab from 'react-bootstrap/Tab';
 import { Row, Nav, Col } from 'react-bootstrap';
+import LazyLoad from 'react-lazyload';
 
 import "./practical.css";
 
 import Aidan from "../../assets/practical/aidan.mp4";
-import PlatformBusiness from "../../assets/practical/platform-business.webm"
+import PlatformBusiness from "../../assets/practical/platform-business.webm";
+import FeedbackExperiment from "../../assets/practical/feedback-experiment.webm";
+import FeedbackReflections from "../../assets/practical/feedback-reflections.webm";
+import IntroducingBLE from "../../assets/practical/introducing-ble.webm";
+import GatewayInstructions from "../../assets/practical/gateway-instructions.webm";
+import GatewayReflections from "../../assets/practical/gateway-reflections.webm";
+import OpenCases from "../../assets/practical/open-cases.webm";
 
 class Practical extends Component {
   render() {
@@ -25,15 +32,67 @@ class Practical extends Component {
                   <Nav.Item>
                     <Nav.Link eventKey="second">platform-business</Nav.Link>
                   </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">feedback-experiment</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="fourth">feedback-reflections</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="fifth">introducing-ble</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="sixth">gateway-instructions</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="seventh">gaateway-reflections</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="eighth">open-cases</Nav.Link>
+                  </Nav.Item>
                 </Nav>
               </Col>
               <Col sm={9}>
                 <Tab.Content>
                   <Tab.Pane eventKey="first">
-                    <video controls src={Aidan} />
+                    <LazyLoad height={480}>
+                      <video controls src={Aidan} />
+                    </LazyLoad>
                   </Tab.Pane>
                   <Tab.Pane eventKey="second">
-                    <video controls src={PlatformBusiness} />
+                    <LazyLoad height={480}>
+                      <video controls src={PlatformBusiness} />
+                    </LazyLoad>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="third">
+                    <LazyLoad height={480}>
+                      <video controls src={FeedbackExperiment} />
+                    </LazyLoad>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="fourth">
+                    <LazyLoad height={480}>
+                      <video controls src={FeedbackReflections} />
+                    </LazyLoad>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="fifth">
+                    <LazyLoad height={480}>
+                      <video controls src={IntroducingBLE} />
+                    </LazyLoad>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="sixth">
+                    <LazyLoad height={480}>
+                      <video controls src={GatewayInstructions} />
+                    </LazyLoad>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="seventh">
+                    <LazyLoad height={480}>
+                      <video controls src={GatewayReflections} />
+                    </LazyLoad>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="eigth">
+                    <LazyLoad height={480}>
+                      <video controls src={OpenCases} />
+                    </LazyLoad>
                   </Tab.Pane>
                 </Tab.Content>
               </Col>
