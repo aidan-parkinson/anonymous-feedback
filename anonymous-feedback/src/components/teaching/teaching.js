1bb6a4420942611b7e976383dd3f97b06085d3f4
aidan-parkinson <aidan.parkinson@gmail.com>
2021-01-09 18:49:00 +0000

    +teaching

--- anonymous-feedback/src/components/teaching/teaching.js
+++ anonymous-feedback/src/components/teaching/teaching.js
@@ -17,14 +17,14 @@ class Studio extends Component {
               <Col sm={3}>
                 <Nav variant="pills" className="flex-column">
                   <Nav.Item>
-                    <Nav.Link eventKey="first">studio-activity</Nav.Link>
+                    <Nav.Link eventKey="first">writing-teaching-cases</Nav.Link>
                   </Nav.Item>
                 </Nav>
               </Col>
               <Col sm={9}>
                 <Tab.Content>
                   <Tab.Pane eventKey="first">
-                    <video controls width="550" src="http://d1kkiml0fde0ur.cloudfront.net/studio/studio-activity.mp4" />
+                    <video controls width="550" src="http://d1kkiml0fde0ur.cloudfront.net/teaching/writing-teaching-cases.pdf" />
                   </Tab.Pane>
                 </Tab.Content>
               </Col>
@@ -33,14 +33,14 @@ class Studio extends Component {
               <Col sm={3}>
                 <Nav variant="pills" className="flex-column">
                   <Nav.Item>
-                    <Nav.Link eventKey="first">studio-city-example</Nav.Link>
+                    <Nav.Link eventKey="first">gla-cities-case</Nav.Link>
                   </Nav.Item>
                 </Nav>
               </Col>
               <Col sm={9}>
                 <Tab.Content>
                   <Tab.Pane eventKey="first">
-                    <iframe controls width="550" src="http://d1kkiml0fde0ur.cloudfront.net/studio/studio-city-example.pdf" />
+                    <iframe controls width="550" src="http://d1kkiml0fde0ur.cloudfront.net/teaching/gla-cities-case.pdf" />
                   </Tab.Pane>
                 </Tab.Content>
               </Col>
@@ -55,4 +55,4 @@ class Studio extends Component {
   }
 }
 
-export default Studio;
+export default Teaching;
